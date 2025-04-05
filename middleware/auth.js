8713ce06d0a4fe680f4e8d71daa834201f9e6420
SarojSahil <sahilsaroj204@gmail.com>
2025-04-05 09:24:21 +0530

    Project Created

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+function auth(req, res, next) {
+    const publicRoutes = [/^\/login$/, /^\/register$/, /^(?:\/admin).*$/]
+
+    for (let r of publicRoutes) {
+        if (r.test(req.path)) {
+            console.log("public url " + req.path)
+            return next();
+        }
+    }
+    if (req.session && req.session.userId) {
+        console.log("private url " + req.path)
+        return next();
+    }
+    else {
+        console.log("error url " + req.path)
+        return res.status(401).send("Unauthorized");
+    }
+}
+
+module.exports = auth;
